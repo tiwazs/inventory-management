0dd7ba274e06c3fb45388cabe1bccf21b0f7ddd1
kaiser24 <damsog38@gmail.com>
2022-10-08 13:04:43 -0500

    Workspaces service

--- dataModels/UserDataModel.ts
+++ dataModels/UserDataModel.ts
@@ -25,3 +25,15 @@ export interface TypeDM extends TypeBaseDM {
     createdAt: Date;
     updatedAt: Date;
 }
+
+/**************************************************|Workspace Data Models|**********************************************/
+export interface WorkspaceBaseDM {
+    userId: string;
+    name: string;
+}
+
+export interface WorkspaceDM extends WorkspaceBaseDM {
+    id: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
---
+++ services/workspaceService.ts
@@ -0,0 +1,58 @@
+import prisma from '../configurations/dbinit';
+import { Workspace } from "@prisma/client";
+import { WorkspaceBaseDM, WorkspaceDM } from "../dataModels/UserDataModel";
+
+export class WorkspaceService {
+    static async findAll(): Promise<Workspace[]> {
+        const workspaces = await prisma.workspace.findMany();
+        return workspaces;
+    }
+
+    static async findById(id: string): Promise<WorkspaceDM | null> {
+        const workspace = await prisma.workspace.findUnique({
+            where: {
+                id: id
+            }
+        });
+        return workspace;
+    }
+
+    static async findByName(name: string): Promise<WorkspaceDM | null> {
+        const workspace = await prisma.workspace.findFirst({
+            where: {
+                name: name
+            }
+        });
+        return workspace;
+    }
+
+    static async create(workspace: WorkspaceBaseDM): Promise<WorkspaceDM> {
+        const workspaceCreated = await prisma.workspace.create({
+            data: {
+                ...workspace
+            }
+        });
+        return workspaceCreated;
+    }
+
+    static async update(id: string, workspace: WorkspaceBaseDM): Promise<WorkspaceDM | null> {
+        const workspaceUpdated = await prisma.workspace.update({
+            where: {
+                id: id
+            },
+            data: {
+                ...workspace
+            }
+        });
+        return workspaceUpdated;
+    }
+
+    static async delete(id: string): Promise<WorkspaceDM | null> {
+        const workspaceDeleted = await prisma.workspace.delete({
+            where: {
+                id: id
+            }
+        });
+        return workspaceDeleted;
+    }
+}
