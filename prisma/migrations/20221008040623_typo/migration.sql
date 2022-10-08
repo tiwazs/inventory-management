5f28417e65e972a59b1353331a02054aac4f4e76
kaiser24 <damsog38@gmail.com>
2022-10-07 23:07:06 -0500

    Fixed a typo

---
+++ prisma/migrations/20221008040623_typo/migration.sql
@@ -0,0 +1,24 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Types` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE `Item` DROP FOREIGN KEY `Item_typesId_fkey`;
+
+-- DropTable
+DROP TABLE `Types`;
+
+-- CreateTable
+CREATE TABLE `Type` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Item` ADD CONSTRAINT `Item_typesId_fkey` FOREIGN KEY (`typesId`) REFERENCES `Type`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
