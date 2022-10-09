260029f6324014148c66c2acbbf36aca1b4ddaa3
kaiser24 <damsog38@gmail.com>
2022-10-09 12:17:01 -0500

    Small fix

--- dataModels/LocationDataModel.ts
+++ dataModels/LocationDataModel.ts
@@ -12,31 +12,3 @@ export interface LocationDM extends LocationBaseDM {
     createdAt: Date;
     updatedAt: Date;
 }
-
-/**************************************************|Item Data Models|**********************************************/
-export interface ItemBaseDM {
-    workspaceId: string;
-    categoryId: string;
-    locationId: string;
-    typesId: string;
-    barcode: string;
-    serialNumber: string;
-    quantity: number;
-    name: string;
-    description: string;
-    wholeSalePriceInd: number;
-    retailPriceInd: number;
-    wholeSalePrice: number;
-    retailPrice: number;
-    length: number;
-    width: number;
-    height: number;
-    weight: number;
-    forSale: boolean;
-}
-
-export interface ItemToCreateDM extends ItemBaseDM {
-    id: string;
-    createdAt: Date;
-    updatedAt: Date;
-}
