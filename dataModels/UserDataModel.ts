58f73c0f5319124b7520f875adb1f3a36e42b728
kaiser24 <damsog38@gmail.com>
2022-10-08 19:08:06 -0500

    Made the category service. still missing the deleting and update which requiere to update the tree

--- dataModels/UserDataModel.ts
+++ dataModels/UserDataModel.ts
@@ -37,3 +37,22 @@ export interface WorkspaceDM extends WorkspaceBaseDM {
     createdAt: Date;
     updatedAt: Date;
 }
+
+/**************************************************|Category Data Models|**********************************************/
+export interface CategoryBaseDM {
+    workspaceId: string
+    name: string
+    description: string
+}
+
+export interface CategoryToCreateDM extends CategoryBaseDM {
+    parentId: string | null | undefined;
+}
+
+export interface CategoryDM extends CategoryBaseDM {
+    id: string;
+    lft: number;
+    rgt: number;
+    createdAt: Date;
+    updatedAt: Date;
+}
---
+++ services/categoryService.ts
@@ -0,0 +1,120 @@
+import prisma from '../configurations/dbinit';
+import { Category } from "@prisma/client";
+import { CategoryBaseDM, CategoryToCreateDM } from '../dataModels/UserDataModel';
+
+export class CategoryService {
+    static async findAll(): Promise<Category[]> {
+        const categories = await prisma.category.findMany();
+        return categories;
+    }
+
+    static async findById(id: string): Promise<Category | null> {
+        const category = await prisma.category.findUnique({
+            where: {
+                id: id
+            }
+        });
+        return category;
+    }
+
+    static async findByName(name: string): Promise<Category | null> {
+        const category = await prisma.category.findFirst({
+            where: {
+                name: name
+            }
+        });
+        return category;
+    }
+    
+
+    /*
+    *  Create a new category
+    *  This table uses a nested set model to store the category tree
+    *  The left and right values are used to store the boundaries of the category in the tree
+    *  within a category boundaries, there can be no other category, and so on.
+    */
+    static async create(category: CategoryToCreateDM): Promise<Category> {
+
+        
+        if(!category.parentId) {            
+            // If the gategory doesn't have a parent, it is a root category
+            const lastCategory = await prisma.category.findFirst({ 
+                where: { workspaceId: category.workspaceId },
+                select: { rgt: true },
+                orderBy: { rgt: 'desc' }
+            });
+
+            if(lastCategory) {
+                const categoryCreated = await prisma.category.create({
+                    data: {
+                        workspaceId: category.workspaceId,
+                        name: category.name,
+                        description: category.description,
+                        lft: lastCategory.rgt + 1,
+                        rgt: lastCategory.rgt + 2
+                    }
+                });
+
+                return categoryCreated;
+            }else{
+                const categoryCreated = await prisma.category.create({
+                    data: {
+                        workspaceId: category.workspaceId,
+                        name: category.name,
+                        description: category.description,
+                        lft: 0,
+                        rgt: 1
+                    }
+                });
+
+                return categoryCreated;
+            }
+        }else{
+            // If the category has a parent, it is a child category.
+            // We need to find the boundaries of the parent category, and insert the new category between them
+            // We also need to update the boundaries of the other categories in the tree
+            const parentCategory = await prisma.category.findUnique({
+                where: {
+                    id: category.parentId
+                }
+            });
+
+            if(parentCategory) {
+                // Update the boundaries of the other categories in the tree.
+                // everything to the right of the parent category, needs to be moved 2 positions to the right
+                // to make space for the new category
+                await prisma.category.updateMany({
+                    where: {
+                        workspaceId: category.workspaceId,
+                        lft: {
+                            gte: parentCategory.lft
+                        }
+                    },
+                    data: {
+                        lft: {
+                            increment: 2
+                        },
+                        rgt: {
+                            increment: 2
+                        }
+                    }
+                });
+
+                const categoryCreated = await prisma.category.create({
+                    data: {
+                        workspaceId: category.workspaceId,
+                        name: category.name,
+                        description: category.description,
+                        lft: parentCategory.lft + 1,
+                        rgt: parentCategory.lft + 2
+                    }
+                });
+
+                return categoryCreated;
+            }else{
+                throw new Error('Parent category not found');
+            }
+        }
+
+    }
+}
