dabb0f9241787fb7425d3c26de46626fcfb0382c
kaiser24 <damsog38@gmail.com>
2022-10-21 21:48:42 -0500

    Added authentication service for register and login

---
+++ services/authenticationService.ts
@@ -0,0 +1,51 @@
+import { User } from "@prisma/client";
+import { UserBaseDM } from "../dataModels/UserDataModel";
+import { Encryptor } from "../lib/encryptor";
+import prisma from '../configurations/dbinit';
+import { UserService } from "./userService";
+import jwt from "jsonwebtoken";
+import logger from "../lib/logger";
+
+export class AuthenticationService {
+
+    static async login(username: string, password: string) {
+        if(!username || !password) { throw new Error("Username and password are required"); }
+
+        const user = await UserService.getEmail(username);
+        if (!user) { throw new Error("User not found");}
+        
+        // Matching password
+        if( !(await Encryptor.matchPassword(password, user.password)) ){ throw new Error("Invalid Credentials"); }
+        
+        logger.debug( `Password matched` );
+        // Creating an access token for the user
+        const token = jwt.sign(
+            {user_id: user.id, username},
+            process.env.TOKEN_KEY!,
+            {expiresIn: "1h"}
+        );
+        logger.debug( `Token generated: ${token}` );
+        
+        // Appends the token to the response | Weird casting from moongoose object to JSON
+        let userJson = JSON.parse(JSON.stringify(user));
+        userJson.token = `Bearer ${token}`;
+
+        return userJson;
+    }
+
+    static async register(user: UserBaseDM): Promise<User> {
+        const encryptPassword:string = await Encryptor.encryptPassword(user.password);
+
+        // Ecrpt the password
+        user.password = encryptPassword;
+
+        const userCreated = await prisma.user.create({
+            data: {
+                ...user
+            }
+        });
+
+        return userCreated;
+    }
+
+}
