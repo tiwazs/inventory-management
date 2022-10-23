1a56175642fc5128a7e09e085ebb0a22c50835f6
kaiser24 <damsog38@gmail.com>
2022-10-23 00:20:46 -0500

    Added IAM access to prisma design

---
+++ prisma/migrations/20221023051803_iam/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE `IAM` (
+    `id` VARCHAR(191) NOT NULL,
+    `workspaceId` VARCHAR(191) NOT NULL,
+    `username` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `role` ENUM('USER', 'ADMIN') NOT NULL DEFAULT 'USER',
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `IAM` ADD CONSTRAINT `IAM_workspaceId_fkey` FOREIGN KEY (`workspaceId`) REFERENCES `Workspace`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
