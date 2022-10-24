221cb0ac4bc2968b77667fcfa5c44ea095e6375b
kaiser24 <damsog38@gmail.com>
2022-10-23 23:16:40 -0500

    Updated user endpoints to read the userif from the token

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -1,11 +1,12 @@
 import express from 'express';
+import { RequestWithUser } from '../dataModels/AuthenticationModels';
 import logger from '../lib/logger';
 const router = express.Router();
 import { UserService } from '../services/userService';
 
 /**
  * @swagger
- * /api/user:
+ * /api/user/getall/{all}:
  *  get:
  *      summary: Return all users
  *      tags: [Users]
@@ -23,14 +24,14 @@ import { UserService } from '../services/userService';
  *                                
  */
 
-router.get('/', async (req, res) => {
+router.get('/getall/:getall', async (req, res) => {
     const users = await UserService.getAll();
     return res.status(200).json(users);
 });
 
 /**
  * @swagger
- * /api/user/{id}:
+ * /api/user/id/{id}:
  *  get:
  *      summary: Return user by id
  *      tags: [Users]
@@ -55,7 +56,7 @@ router.get('/', async (req, res) => {
  *                                                               
  */
 
-router.get('/:id', async (req, res) => {
+router.get('/id/:id', async (req, res) => {
     const { id } = req.params;
     try{
         const user = await UserService.getById(id);
@@ -110,7 +111,40 @@ router.get('/email/:email', async (req, res) => {
 
 /**
  * @swagger
- * /api/user/{id}:
+ * /api/user:
+ *  get:
+ *      summary: Return user info
+ *      tags: [Users]
+ *      security:
+ *          - bearerAuth: []
+ *      responses:
+ *          200:
+ *              description: User
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/user'
+ *          404:
+ *              description: User not found
+ *                                                               
+ */
+
+ router.get('/', async (req: RequestWithUser, res) => {
+    const { user } = req;
+    try{
+        const userRead = await UserService.getById(user.userId);
+        if(userRead) return res.status(200).json(userRead);
+
+        return res.status(404).json();
+    }catch(error: any){
+        logger.error(error.message);
+        return res.status(500).json({error: error.message});
+    }
+});
+
+/**
+ * @swagger
+ * /api/user/id/{id}:
  *  put:
  *      summary: Updates user
  *      tags: [Users]
@@ -141,7 +175,7 @@ router.get('/email/:email', async (req, res) => {
  *                                
  */
 
-router.put('/:id', async (req, res) => {
+router.put('/id/:id', async (req, res) => {
     const { id } = req.params;
     const { body } = req;
     try{
@@ -157,7 +191,47 @@ router.put('/:id', async (req, res) => {
 
 /**
  * @swagger
- * /api/user/{id}:
+ * /api/user:
+ *  put:
+ *      summary: Updates user
+ *      tags: [Users]
+ *      security:
+ *          - bearerAuth: []
+ *      requestBody:
+ *          required: true
+ *          content: 
+ *              application/json:
+ *                  schema:
+ *                      $ref: '#/components/schemas/userToUpdate'
+ *      responses:
+ *          200:
+ *              description: If operation was succesful
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/user'
+ *          404:
+ *              description: User not found
+ *                                
+ */
+
+ router.put('/', async (req: RequestWithUser, res) => {
+    const { body } = req;
+    const { user } = req;
+    try{
+        const userUdated = await UserService.update(user.userId, body);
+        if(userUdated) return res.status(200).json(userUdated);
+        
+        return res.status(404).json();
+    }catch(error: any){
+        logger.error(error.message);
+        return res.status(500).json({error: error.message});
+    }
+});
+
+/**
+ * @swagger
+ * /api/user/id/{id}:
  *  delete:
  *      summary: Deletes a user by id
  *      tags: [Users]
@@ -182,11 +256,44 @@ router.put('/:id', async (req, res) => {
  *                                
  */
 
-router.delete('/:id', async (req, res) => {
+router.delete('/id/:id', async (req, res) => {
     const { id } = req.params;
     try{
         const user = await UserService.delete(id);
         if(user) return res.status(200).json(user);
+
+        return res.status(404).json();
+    }catch(error: any){
+        logger.error(error.message);
+        return res.status(500).json({error: error.message});
+    }
+});
+
+/**
+ * @swagger
+ * /api/user:
+ *  delete:
+ *      summary: Deletes a user by id
+ *      tags: [Users]
+ *      security:
+ *          - bearerAuth: []
+ *      responses:
+ *          200:
+ *              description: If operation was succesful
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: string
+ *          404:
+ *              description: User not found
+ *                                
+ */
+
+ router.delete('/', async (req: RequestWithUser, res) => {
+    const { user } = req;
+    try{
+        const userDeleted = await UserService.delete(user.userId);
+        if(userDeleted) return res.status(200).json(userDeleted);
 
         return res.status(404).json();
     }catch(error: any){
